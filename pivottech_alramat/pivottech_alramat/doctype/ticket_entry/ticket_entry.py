83bd70763eb9d1a68190cfac8c8eb5c8eee7f89d
osamaerksosi <osama.ereksosi@gmail.com>
2022-05-22 15:02:14 +0200

    Complete all Ticket Entry Fields , add progress bar

--- pivottech_alramat/pivottech_alramat/doctype/ticket_entry/ticket_entry.py
+++ pivottech_alramat/pivottech_alramat/doctype/ticket_entry/ticket_entry.py
@@ -1,28 +1,53 @@
 # Copyright (c) 2022, Pivottech and contributors
 # For license information, please see license.txt
 
+from itertools import count
 import frappe
 from frappe.model.document import Document
 from frappe.utils.xlsxutils import (
 	read_xls_file_from_attached_file,
 	read_xlsx_file_from_attached_file,
 )
-from frappe.utils import flt
+from frappe.utils import flt, add_days, formatdate
+from frappe import _
 
 class ticketEntry(Document):
-	pass
+	def has_back(self):
+		#check if this ticket has back(return) journey or not 
+		return self.back_date and self.back_routing and self.back_flight_no
+	
+	def insert_route(self, route, last_routing):
+		date_arr = route[3:]
+		date_arr.reverse()
+		date_str = " ".join(date_arr)
+		if self.has_back() or last_routing == route[2]:
+			self.back_flight_no = self.back_flight_no + "-" + route[1] if self.back_flight_no else route[1]
+			self.update_routing(route[2], True)
+			self.back_date = self.back_date + " " + date_str if (self.back_date and self.back_date != date_str) else date_str
+		else:
+			self.departure_flight_no = self.departure_flight_no + "-" + route[1] if self.departure_flight_no else route[1]
+			self.update_routing(route[2])
+			self.departure_date = self.departure_date + " " + date_str if (self.departure_date and self.departure_date != date_str) else date_str
 
-
+	def update_routing (self, routing, is_back=False):
+		if is_back:
+			self.back_routing = self.back_routing.strip()[:-3] + routing if self.back_routing else routing
+		else:
+			self.departure_routing = self.departure_routing.strip()[:-3] + routing if self.departure_routing else routing
+			
 @frappe.whitelist()
 def insert_tickets(filepath):
 	data = read_excel(filepath)
+	count = 0
 	for row in data:
+		count += 1
+		frappe.publish_progress(count*100/len(data), title=_("Importing Ticket Entries"), description = row["pnr"])
 		d = frappe._dict(row)
 		ticket_entry = frappe.new_doc("ticket Entry")
 		if d.pnr:
 			d.e_ticket = str(d.e_ticket)
 			e_tickets = d.e_ticket.split("-")
-			etickets_float = [flt(et[3:]) for et in e_tickets]
+			etickets_float = [flt(et[3:],0) for et in e_tickets]
 			#pnr
 			ticket_entry.pnr = d.pnr
 			#E-Ticket
@@ -33,17 +58,34 @@ def insert_tickets(filepath):
 			if not frappe.get_list("passenger name", filters={"name": d.passenger_name}):
 				frappe.get_doc({
 					"doctype": "passenger name",
-					"passenger": d.passenger_name
+					"passenger": d.passenger_name,
+					"nationality": d.natationality
 				}).insert()
 			ticket_entry.passenger = d.passenger_name
-			#Payment Mode
-			ticket_entry.payment_mode = d.payment_mode
+			#routing
+			routings = d.routing.split(";")
+			last_routing = None
+			for r in routings:
+				last_routing = handle_routing(ticket_entry, last_routing, r.strip())
+			#standard fields
+			standard_fields = ["fare_amount", "payment_mode", "tax_amount", "charge_amount", "modify_amount", "total_amount", "sales", "pax_name", "natationality"]
+			for sf in standard_fields:
+				setattr(ticket_entry, sf, d[sf])
+			#non standard fields
+			if type(d.payment_date) is not str:
+				ticket_entry.payment_date = add_days("1/1/1900", d.payment_date-2)
+			else:
+				formatdate(d.payment_date, "dd-MMM-yy")
+			ticket_entry.user = d.user_name
+			ticket_entry.refund_amount = d.re_found_amount
+			ticket_entry.supp_com = d["supp_com%"]
+			ticket_entry.sales_com = d["sales_com%"]
+			ticket_entry.currency = d["curr."]
+			ticket_entry.conversion_rate = d.rate
+
 			ticket_entry.insert()
 			
-
-
-
-
+			
 
 def read_excel(filepath):
 	file_doc, extension = get_file(filepath)
@@ -84,3 +126,26 @@ def generate_data_from_excel(file_doc, extension, as_dict=False):
 			data.append(row)
 
 	return data
+
+def handle_routing(ticket_entry, last_routing, routing):
+	route = routing.split(" ")
+	route.reverse()
+	if route[0] == "CNX":
+		return last_routing
+
+	ticket_entry.insert_route(route, reverse_routing(last_routing))
+	return route[2]
+	
+
+
+	
+#08-APR-22 DAM/NJF 6Q511 CNX ; OPEN RETURN  NJF/DAM 6Q512 CNX
+def reverse_routing(r):
+#revers routing to check back route 
+#ex: DAM/SHJ => SHL/Dam
+	if not r:
+		return ""
+	rs = r.split("/")
+	rs.reverse()
+	return "/".join(rs)
+	
